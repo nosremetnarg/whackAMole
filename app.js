87bfe4ec589cc6cbd21074d16a185f6a4828d6ff
nosremetnarg <grant.alex.emerson@gmail.com>
2020-06-21 15:57:13 -0500

    connecting to github

--- app.js
+++ app.js
@@ -1,49 +1,51 @@
-const square = document.querySelectorAll('.square')
-const mole = document.querySelectorAll('.mole')
-const timeLeft = document.querySelector('#time-left')
-let score = document.querySelector('#score')
-
-let result = 0
-let currentTime = timeLeft.textContent
-
-function randomSquare() {
-  square.forEach(className => {
-    className.classList.remove('mole')
-  })
-  let randomPosition = square[Math.floor(Math.random() * 9)]
-  randomPosition.classList.add('mole')
-
-  //assign the id of the randomPosition to hitPosition for us to use later
-  hitPosition = randomPosition.id
-}
-
-square.forEach(id => {
-  id.addEventListener('mouseup', () => {
-    if(id.id === hitPosition){
-      result = result + 1
-      score.textContent = result
-      hitPosition=null
-    }
-  })
-})
 
 
-function moveMole() {
-  let timerId = null
-  timerId = setInterval(randomSquare, 500)
-}
+// const square = document.querySelectorAll('.square')
+// const mole = document.querySelectorAll('.mole')
+// const timeLeft = document.querySelector('#time-left')
+// let score = document.querySelector('#score')
 
-moveMole()
+// let result = 0
+// let currentTime = timeLeft.textContent
+
+// function randomSquare() {
+//   square.forEach(className => {
+//     className.classList.remove('mole')
+//   })
+//   let randomPosition = square[Math.floor(Math.random() * 9)]
+//   randomPosition.classList.add('mole')
+
+//   //assign the id of the randomPosition to hitPosition for us to use later
+//   hitPosition = randomPosition.id
+// }
+
+// square.forEach(id => {
+//   id.addEventListener('mouseup', () => {
+//     if(id.id === hitPosition){
+//       result = result + 1
+//       score.textContent = result
+//       hitPosition=null
+//     }
+//   })
+// })
 
 
-function countDown() {
-  currentTime--
-  timeLeft.textContent = currentTime
+// function moveMole() {
+//   let timerId = null
+//   timerId = setInterval(randomSquare, 500)
+// }
 
-  if(currentTime === 0 ) {
-    clearInterval(timerId)
-    alert('GAME OVER! Your final score is' + result)
-  }
-}
+// moveMole()
 
-let timerId = setInterval(countDown, 1000)
+
+// function countDown() {
+//   currentTime--
+//   timeLeft.textContent = currentTime
+
+//   if(currentTime === 0 ) {
+//     clearInterval(timerId)
+//     alert('GAME OVER! Your final score is' + result)
+//   }
+// }
+
+// let timerId = setInterval(countDown, 1000)
